205048def86bce2fb0070cb7f934d65ffa96d0cc
miirsery <miirsery.main@gmail.com>
2022-05-11 01:14:27 +0300

    added header component

---
+++ components/Common/CommonHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  const emit = defineEmits([
+    'toggleModal'
+  ])
+
+  const handleToggleModal = () => {
+      emit('toggleModal')
+  }
+</script>
+
+<template>
+  <div class="common-header">
+    <div class="common-header__logo">
+      <img src="" alt="logo" />
+    </div>
+    <ul class="common-header__menu">
+      <li class="common-header__item">
+        Account
+      </li>
+      <li class="common-header__item">
+        Go to stars
+      </li>
+    </ul>
+    <div class="common-header__avatar" @click="handleToggleModal">
+      <img src="" alt="avatar">
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    useMeta({
+        title: `Main page`,
+        meta: [
+            {
+                name: "viewport",
+                content: "width=device-width",
+            },
+        ],
+    })
+
+    const authModalVisible = ref(false)
+
+    const toggleModal = () => {
+      authModalVisible.value = !authModalVisible.value
+    }
+</script>
+    
+<template>
+    <common-header @toggle-modal="toggleModal"/>
+</template>
+    
