9ef46e67206c2a5f678e473759b8262b0a6955f9
Cxatek18 <griga2888@mail.ru>
2022-05-10 19:20:08 +0500

    Added functionality for changing username and avatar user

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include, re_path
 from rest_framework_jwt.views import obtain_jwt_token
 from .views import (
-    UserCreateAPIView,
+    UserCreateApiView,
+    UserUpdateApiView,
 )
 
 urlpatterns = [
     path('register/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('token-create/', obtain_jwt_token, name='obtain_jwt_token'),
-    path('authorization/', UserCreateAPIView.as_view()),
+    path('authorization/', UserCreateApiView.as_view()),
+    path('update/<int:pk>/', UserUpdateApiView.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -1,12 +1,13 @@
 # from django.shortcuts import render
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 from .models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-class UserCreateAPIView(APIView):
+class UserCreateApiView(APIView):
     queryset = User.objects.filter(is_active=True)
     permission_classes = (IsAuthenticated,)
 
@@ -14,3 +15,9 @@ class UserCreateAPIView(APIView):
         request.data.copy()
         return Response(UserSerializer(request.user,
                                        context={"request": request}).data)
+
+
+class UserUpdateApiView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
