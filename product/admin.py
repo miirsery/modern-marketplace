0a50cbfbd2d5d651133702f94d804d16fa38e765
Cxatek18 <griga2888@mail.ru>
2022-05-12 18:10:28 +0500

    add middleware corsheader

--- product/admin.py
+++ product/admin.py
@@ -8,11 +8,17 @@ from .models import (
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title',)
+    list_display = (
+        'id', 'title',
+        'price_now', 'created_at',
+        'updated_at', 'moderation',
+        'guarantee', 'quantity',
+        'year', 'status', 'category',
+    )
     list_display_links = ('id', 'title',)
-    search_fields = ('id', 'title',)
-    # list_editable = ('moderation',)
-    list_filter = ('moderation',)
+    search_fields = ('id', 'title', 'category',)
+    list_editable = ('moderation',)
+    list_filter = ('moderation', 'status',)
     prepopulated_fields = {'slug': ('title',)}
 
 
