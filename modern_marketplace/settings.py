3ae61f502305791135e56b67d64ef1af8e5f73d0
Cxatek18 <griga2888@mail.ru>
2022-05-17 22:18:17 +0500

    delivery of the project to postgresql

--- modern_marketplace/settings.py
+++ modern_marketplace/settings.py
@@ -90,8 +90,10 @@ WSGI_APPLICATION = 'modern_marketplace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('BASE_NAME'),
+        'USER': os.getenv('USERNAME_DB'),
+        'PASSWORD': os.getenv('USERPASSWORD_DB'),
     }
 }
 
